e5514bac94bf8b04925099d98725c8861c661f36
jos <julian.osmer@outlook.de>
2020-01-27 16:11:49 +0100

    changes

--- src/main/java/josephus/Josephus.java
+++ src/main/java/josephus/Josephus.java
@@ -1,9 +1,21 @@
 package josephus;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Josephus {
     public static <T> List<T> josephusPermutation(final List<T> items, final int k) {
-        return null;
+        List<T> result =  new ArrayList<>();
+        List<T> josephus =  new ArrayList<>(items);
+        int toKill = k - 1;
+        Object[] objects = items.toArray();
+        while (!josephus.isEmpty()) {
+            Object killed = objects[toKill];
+            josephus.remove(killed);
+            result.add((T) killed);
+            toKill += k;
+        }
+        result.addAll(josephus);
+        return result;
     }
 }
