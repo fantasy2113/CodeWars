0b4d0f0e72340558f25fd8ff5bdfb32bdc5f1270
jos <julian.osmer@outlook.de>
2020-01-27 14:18:16 +0100

    add

---
+++ src/test/java/helloWorld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package helloWorld;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void get() {
+        Assert.assertEquals("HelloWorld!", HelloWorld.get());
+    }
+}
