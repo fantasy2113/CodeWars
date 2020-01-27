dde96b18ce15364bef6c4d36da2255f6a52d4c14
jos <julian.osmer@outlook.de>
2020-01-27 15:27:16 +0100

    add

---
+++ src/main/java/josephus/Josephus.java
@@ -0,0 +1,9 @@
+package josephus;
+
+import java.util.List;
+
+public class Josephus {
+    public static <T> List<T> josephusPermutation(final List<T> items, final int k) {
+        return null;
+    }
+}
