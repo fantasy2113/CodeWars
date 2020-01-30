c64721b3f9c684a4135d420edcdbdb1f4eba7d67
jos <julian.osmer@outlook.de>
2020-01-30 18:21:42 +0100

    changes

--- src/main/java/josephus/Josephus.java
+++ src/main/java/josephus/Josephus.java
@@ -12,17 +12,20 @@ public class Josephus {
         List<T> josephus = new ArrayList<>(items);
         int next = k;
         int offset = 1;
+        int start = k - 1;
 
 
         Object[] objects = items.toArray();
-        final int length = objects.length - 1;
+        final int n = objects.length;
         while (josephus.size() != 1) {
-            for (int kill = next - offset; kill < objects.length; kill += next) {
+            int store = -1;
+            for (int kill = start; kill < objects.length; kill += next) {
                 killed.add((T) objects[kill]);
                 josephus.remove(objects[kill]);
+                store = kill;
             }
+            start = (store + next) - n;
             next += next;
-            offset += 1;
         }
         killed.addAll(josephus);
         return killed;
